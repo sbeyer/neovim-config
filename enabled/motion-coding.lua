66f3172103e3a3aecb333feea30acd1249a570ff
Stephan Beyer <s-beyer@gmx.net>
2021-11-01 01:54:19 +0100

    Replace CamelCaseMotion by vim-wordmotion
    This allows to use w/b/e/ge directly instead of
    using the leader key. We however keep the mapleader
    setting for now.

--- enabled/motion-coding.lua
+++
@@ -1,8 +0,0 @@
-return {
-  -- <leader>w/<leader>b jumps but aware of camelCase and snake_case
-  'bkad/CamelCaseMotion',
-
-  config = function()
-    vim.g.camelcasemotion_key = '<leader>'
-  end
-}
---
+++ enabled/motion.lua
@@ -0,0 +1,4 @@
+return {
+  -- make w/b/e consider camelCase and snake_case
+  'chaoren/vim-wordmotion',
+}
