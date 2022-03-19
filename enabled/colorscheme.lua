ff2138cee91fe01e589ce08c503aa3998ac53ab3
Stephan Beyer <s-beyer@gmx.net>
2022-03-19 03:49:41 +0100

    Change colorscheme to base16-seti

--- enabled/colorscheme.lua
+++ enabled/colorscheme.lua
@@ -1,10 +1,16 @@
 return {
   plug = {
-    'bluz71/vim-nightfly-guicolors'
+    'chriskempson/base16-vim'
   },
 
   post = function()
     vim.o.termguicolors = true
-    vim.cmd('colorscheme nightfly')
+    -- Some example colorschemes from base16-vim that I liked, i.e., dark themes
+    -- that have a good contrast and are colorful without being ugly
+    -- vim.cmd('colorscheme base16-brewer')
+    -- vim.cmd('colorscheme base16-chalk')
+    -- vim.cmd('colorscheme base16-classic-dark')
+    -- vim.cmd('colorscheme base16-helios')
+    vim.cmd('colorscheme base16-seti')
   end
 }
