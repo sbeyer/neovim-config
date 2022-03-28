c9dc8ff6bc434bfc00cc8ea7d6a8e7dadf23ad06
Stephan Beyer <s-beyer@gmx.net>
2022-03-28 02:25:07 +0200

    Add some git functionality now missing from not using fugitive

--- init.lua
+++ init.lua
@@ -31,6 +31,12 @@ local config = {
 
     -- disable displaying math (etc) for TeX and LaTeX
     vim.cmd('autocmd FileType tex set conceallevel=0')
+
+    ----- Mappings
+    local opts = { noremap = true, silent = true }
+    local map = vim.api.nvim_set_keymap
+    map("n", "<leader>gR", "<cmd>!git rm -f %<cr>", opts)
+    map("n", "<leader>gw", "<cmd>!git add %<cr>", opts)
   end
 }
 
