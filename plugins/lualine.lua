158154bc9dd0567a1767c0092bb2712004d29135
Stephan Beyer <s-beyer@gmx.net>
2022-04-21 00:45:54 +0200

    Configure lualine
    It's debatable if this is prettier now, but it's more functional for me.

--- plugins/lualine.lua
+++ plugins/lualine.lua
@@ -1,29 +1,67 @@
+local ll_diff = {
+  "diff",
+  symbols = { added = " ", modified = "柳", removed = " " },
+}
+
+local l_git = { 'branch', ll_diff }
+
+local ll_diag = {
+  "diagnostics",
+  sources = { "nvim_diagnostic" },
+  symbols = { error = " ", warn = " ", info = " ", hint = " " },
+}
+
+local l_fileinfo = {
+  'filesize',
+  'encoding',
+  'fileformat',
+}
+
+local l_code = {
+  'filetype',
+  ll_diag,
+}
+
+local l_location = {
+  {
+    'location',
+    padding = { left = 0, right = 0 },
+    separator = { left = '', right = nil },
+  },
+  {
+    require("core.status").progress_bar,
+    padding = { left = 0, right = 0 },
+    separator = { left = nil, right = nil },
+    color = { fg = '#000000' }
+  },
+}
+
 return {
   -- Default lualine configuration as of 2022-03-28
   options = {
     icons_enabled = true,
     theme = 'auto',
-    component_separators = { left = '', right = ''},
-    section_separators = { left = '', right = ''},
-    disabled_filetypes = {},
-    always_divide_middle = true,
+    component_separators = { left = '', right = '' },
+    section_separators = { left = '', right = '' },
+    disabled_filetypes = { "NvimTree", "neo-tree", "dashboard", "Outline" },
+    always_divide_middle = false,
     globalstatus = false,
   },
   sections = {
-    lualine_a = {'mode'},
-    lualine_b = {'branch', 'diff', 'diagnostics'},
-    lualine_c = {'filename'},
-    lualine_x = {'encoding', 'fileformat', 'filetype'},
-    lualine_y = {'progress'},
-    lualine_z = {'location'}
+    lualine_a = { 'mode' },
+    lualine_b = { { 'filename', path = 1 } },
+    lualine_c = l_git,
+    lualine_x = l_fileinfo,
+    lualine_y = l_code,
+    lualine_z = l_location,
   },
   inactive_sections = {
-    lualine_a = {},
+    lualine_a = { { 'filename', path = 2, color = 'StatusLineTerm' } },
     lualine_b = {},
-    lualine_c = {'filename'},
-    lualine_x = {'location'},
-    lualine_y = {},
-    lualine_z = {}
+    lualine_c = l_git,
+    lualine_x = l_fileinfo,
+    lualine_y = l_code,
+    lualine_z = l_location,
   },
   tabline = {},
   extensions = {}
