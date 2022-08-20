e66acb4b7b416742c462997c6c8bb3dea7b16d24
Stephan Beyer <s-beyer@gmx.net>
2022-08-20 05:08:19 +0200

    Remove/replace tex-related autocmds

--- polish.lua
+++ polish.lua
@@ -1,16 +1,10 @@
 return function()
   ----- File-type specific configuration
 
-  -- Recognize qml
   vim.filetype.add {
     extension = {
-      qml = "qmljs",
+      qml = "qmljs", -- recognize qml
+      tex = "tex", -- always ft=tex (LaTeX), never ft=plaintex (TeX)
     }
   }
-
-  -- I never deal with plain tex, always LaTeX... respect that!
-  vim.cmd('autocmd FileType plaintex set ft=tex')
-
-  -- disable displaying math (etc) for TeX and LaTeX
-  vim.cmd('autocmd FileType tex set conceallevel=0')
 end
