92fe85a62f161035c5d581b7f80617e671ca14bf
Stephan Beyer <s-beyer@gmx.net>
2022-08-20 05:05:19 +0200

    Extract polish to polish.lua, remove init.lua

---
+++ polish.lua
@@ -0,0 +1,16 @@
+return function()
+  ----- File-type specific configuration
+
+  -- Recognize qml
+  vim.filetype.add {
+    extension = {
+      qml = "qmljs",
+    }
+  }
+
+  -- I never deal with plain tex, always LaTeX... respect that!
+  vim.cmd('autocmd FileType plaintex set ft=tex')
+
+  -- disable displaying math (etc) for TeX and LaTeX
+  vim.cmd('autocmd FileType tex set conceallevel=0')
+end
