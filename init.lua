52cca035bf461a71efaa56eff5f9f53b56df3d01
Stephan Beyer <s-beyer@gmx.net>
2022-08-20 02:30:13 +0200

    Extract init.lua's options into options.lua

--- init.lua
+++ init.lua
@@ -1,32 +1,6 @@
 local config = {
   colorscheme = "material",
 
-  options = {
-    opt = {
-      -- status line everywhere
-      laststatus = 2,
-
-      -- completion behavior in command mode
-      wildmode = 'longest:full,full',
-
-      -- disable weird completion behavior regarding suffixes
-      suffixes = '',
-
-      -- enable mouse only for Normal and Visual mode only
-      mouse = 'nv',
-
-      -- make whitespace visible
-      list = true,
-      listchars = 'tab:^`,trail:~,extends:\\,precedes:»',
-
-      -- make implicit linebreaks visible
-      wrap = true,
-      showbreak = '  «',
-      breakindent = true,
-      breakindentopt = 'shift:2,sbr',
-    }
-  },
-
   polish = function()
     ----- File-type specific configuration
 
