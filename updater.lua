0420900ab89c70fbfaa1e03daa71ea00b2249b5b
Stephan Beyer <s-beyer@gmx.net>
2023-01-11 01:33:22 +0100

    Remove updater.lua to get back on the stable channel

--- updater.lua
+++
@@ -1,7 +0,0 @@
-return {
-  channel = "nightly",
-  -- disable automatically reloading AstroNvim after an update
-  auto_reload = false,
-  -- disable automatically quitting AstroNvim after an update
-  auto_quit = false,
-}
