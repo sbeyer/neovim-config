08e5d822a079fe9f1979893c3af78ccee2780f3d
Stephan Beyer <s-beyer@gmx.net>
2023-01-12 01:23:02 +0100

    Remove file_info from statusline

--- plugins/heirline.lua
+++ plugins/heirline.lua
@@ -5,13 +5,13 @@ return function(config)
   -- add mode text
   status[1] = astronvim.status.component.mode { mode_text = { padding = { left = 1, right = 1 } } }
 
-  -- swap file info and git branch to have git stuff together
-  status[2], status[3] = status[3], status[2]
+  -- remove file info
+  table.remove(status, 3)
 
   -- move diagnostics (index 5) before lsp info (index 9)
-  local diag = status[5]
-  table.remove(status, 5)
-  table.insert(status, 8, diag)
+  local diag = status[4]
+  table.remove(status, 4)
+  table.insert(status, 7, diag)
 
   -- adapt winbar: filename please!
   local winbar = config[2]
