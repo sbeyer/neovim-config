7b19603b79b158f665a71b51f0bf7bc85411a9df
Stephan Beyer <s-beyer@gmx.net>
2023-01-19 02:37:22 +0100

    Add further mappings for telescope pickers

--- which-key/register.lua
+++ which-key/register.lua
@@ -14,6 +14,11 @@ return {
         name = "Find",
         B = { "<cmd>Telescope bibtex<cr>", "Search bibliography" },
         e = { "<cmd>Telescope file_browser<cr>", "File explorer" },
+        M = { "<cmd>Telescope man_pages<cr>", "Search man pages" },
+        k = { "<cmd>Telescope keymaps<cr>", "Search vim keymaps" },
+        n = { "<cmd>Telescope notify<cr>", "Search vim notifications" },
+        r = { "<cmd>Telescope registers<cr>", "Search vim registers" },
+        C = { "<cmd>Telescope colorscheme<cr>", "Search vim colorschemes" },
       },
     }
   },
