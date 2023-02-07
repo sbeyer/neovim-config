d0a01efc764383e2e40d82c8c8defbcebcb37d2b
Stephan Beyer <s-beyer@gmx.net>
2023-02-07 23:25:01 +0100

    Make telescope-file-browser open in dir of current buffer

--- plugins/telescope.lua
+++ plugins/telescope.lua
@@ -3,6 +3,7 @@ local fb_actions = require("telescope").extensions.file_browser.actions
 return {
   extensions = {
     file_browser = {
+      path = "%:p:h", -- run from directory of current buffer
       mappings = {
         i = {
           ["<C-z>"] = fb_actions.toggle_hidden,
