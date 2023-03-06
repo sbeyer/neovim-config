a61c93709e0a886e227375fa76f0f9ff62c50f07
Stephan Beyer <s-beyer@gmx.net>
2023-03-06 03:06:03 +0100

    Reactivate material colorscheme

--- plugins/colorscheme.lua
+++ plugins/colorscheme.lua
@@ -1,8 +1,8 @@
 return {
-  setup = function()
+  'marko-cerovac/material.nvim',
+  init = function()
     vim.g.material_style = 'darker'
   end,
-
   config = function()
     require('material').setup {
       high_visibility = {
