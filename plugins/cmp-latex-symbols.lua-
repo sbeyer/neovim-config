a55092367a9600b973bf1183c30eb504c1adb60d
Stephan Beyer <s-beyer@gmx.net>
2023-03-07 01:25:57 +0100

    Reactivate cmp-latex-symbols and finalize plugins/latex.lua

--- plugins/cmp-latex-symbols.lua-
+++
@@ -1,6 +0,0 @@
-return {
-  after = "nvim-cmp",
-  config = function()
-    astronvim.add_user_cmp_source "latex_symbols"
-  end,
-}
--- plugins/latex.lua
+++ plugins/latex.lua
@@ -1,4 +1,47 @@
 return {
+  -- Ensure latex-related treesitter parsers are installed
+  {
+    "nvim-treesitter/nvim-treesitter",
+    opts = function(_, opts)
+      if not opts.ensure_installed then
+        opts.ensure_installed = {}
+      elseif opts.ensure_installed == "all" then
+        return
+      end
+
+      vim.list_extend(opts.ensure_installed, { "latex", "bibtex" })
+    end,
+  },
+
+  -- Ensure texlab LSP is installed
+  {
+    "williamboman/mason-lspconfig.nvim",
+    opts = function(_, opts)
+      if not opts.ensure_installed then
+        opts.ensure_installed = {}
+      end
+
+      vim.list_extend(opts.ensure_installed, { "texlab" })
+    end,
+  },
+
+  -- cmp source for latex symbols
+  {
+    "hrsh7th/nvim-cmp",
+    dependencies = { "kdheepak/cmp-latex-symbols" },
+    opts = function(_, opts)
+      if not opts.sources then
+        opts.sources = {}
+      end
+
+      table.insert(opts.sources, {
+        name = "latex_symbols",
+        option = { strategy = 2 }, -- show and insert the command
+        priority = 700
+      })
+    end,
+  },
+
   -- BibTeX finder in telescope
   {
     "nvim-telescope/telescope-bibtex.nvim",
