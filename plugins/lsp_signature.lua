e8293021aac2ef38ddfa30b60ff005cedf67f85a
Stephan Beyer <s-beyer@gmx.net>
2023-03-10 02:19:27 +0100

    Reactivate lsp_signature plugin

---
+++ plugins/lsp_signature.lua
@@ -0,0 +1,13 @@
+return {
+  -- LSP function signature awesomeness
+  {
+    "ray-x/lsp_signature.nvim",
+    event = "BufRead",
+    config = function()
+      require('lsp_signature').setup {
+        toggle_key = '<M-x>',
+        select_signature_key = '<M-n>',
+      }
+    end
+  }
+}
