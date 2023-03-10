c218730748d8fa198da5b2c1fb969fbbb8d013b3
Stephan Beyer <s-beyer@gmx.net>
2023-03-10 03:08:08 +0100

    Fix telescope-file-browser keys

--- plugins/telescope-file-browser.lua
+++ plugins/telescope-file-browser.lua
@@ -2,7 +2,32 @@ return {
   -- File browser via telescope
   {
     "nvim-telescope/telescope-file-browser.nvim",
-    config = function() require("telescope").load_extension "file_browser" end,
+    dependencies = {
+      "nvim-telescope/telescope.nvim",
+      "nvim-lua/plenary.nvim",
+    },
+    config = function()
+      local actions = require "telescope.actions"
+      local fb_actions = require("telescope").extensions.file_browser.actions
+      require("telescope").setup {
+        extensions = {
+          file_browser = {
+            path = "%:p:h", -- run from directory of current buffer
+            mappings = {
+              i = {
+                ["<C-t>"] = actions.select_tab,
+                ["<C-z>"] = fb_actions.change_cwd,
+              },
+              n = {
+                t = actions.select_tab,
+                z = fb_actions.change_cwd,
+              },
+            },
+          },
+        },
+      }
+      require("telescope").load_extension "file_browser"
+    end,
     keys = {
       {
         "<leader>fe",
