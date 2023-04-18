33547dc1f5586961f30270f5fede5eed59717000
Stephan Beyer <s-beyer@gmx.net>
2023-04-18 02:35:49 +0200

    Fix formatoption configuration

--- options.lua
+++ options.lua
@@ -18,7 +18,5 @@ return {
     showbreak = "  «",
     breakindent = true,
     breakindentopt = "shift:2,sbr",
-    -- some reasonable formatoptions defaults (may be overwritten by filetype.lua)
-    formatoptions = "cjlqr/",
   },
 }
--- polish.lua
+++ polish.lua
@@ -1,7 +1,9 @@
 return function()
   vim.api.nvim_create_autocmd({ "FileType" }, {
     callback = function()
-      vim.opt.formatoptions:remove({ "o" })
+      vim.opt.formatoptions:append("r")
+      vim.opt.formatoptions:append("/")
+      vim.opt.formatoptions:remove("o")
     end,
   })
 
